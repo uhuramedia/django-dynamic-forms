c498d590426ce7196d7cf8707c2a45d92f01c903
Jose Luis Romero <jose.romero@uhura.de>
2015-09-01 10:22:10 +0200

    removed meta option excluding required from boolean dynamic field

--- dynamic_forms/formfields.py
+++ dynamic_forms/formfields.py
@@ -190,9 +190,6 @@ class BooleanField(BaseDynamicFormField):
     cls = 'django.forms.BooleanField'
     display_type = _('Boolean')
 
-    class Meta:
-        _exclude = ('required',)
-
 
 @dynamic_form_field
 class ChoiceField(BaseDynamicFormField):
